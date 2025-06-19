59c5720996d6ca251d132b0b895fb1fc84d47e24
MCF - KEN ANGELO C. ARDIENTE <kcardiente@n-pax.com>
2025-06-19 13:18:48 +0800

    dpr kd table template

--- src/app/dpr-kd/page.tsx
+++ src/app/dpr-kd/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import DprKdSearchParameters from '@/components/DprKdSearchParameters' // adjust path as needed
+import DprKdTable from '@/components/DprKdTable'
 
 const Page = () => {
     return (
@@ -7,6 +8,8 @@ const Page = () => {
             <h1 className="text-2xl font-bold mb-4">DPR KD</h1>
             <p className="mb-4">View the daily production report of the Palletizing process.</p>
             <DprKdSearchParameters />
+
+            <DprKdTable />
         </div>
     )
 }
